eb21978094130e7c2becd3bde2b828fc998e360b
Kunal Gunwal <kunalgunwal05@gmail.com>
2024-12-27 12:59:43 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -13,8 +13,14 @@ app.use(express.json({limit: "16kb"})) // limit, extended wagarah extra options 
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public")) // koi bhi public cheeze rakhne ke liye
 
- app.use(cookieParser())
+app.use(cookieParser())
 
+//routes import
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
 
 export { app }
 
